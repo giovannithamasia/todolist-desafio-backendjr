9ef8f7511ca00b042a00aeb1ea4dc2bb53811cb2
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-01-06 16:39:19 -0300

    init backend

---
+++ src/main/java/com/thamasia/todolist/Application.java
@@ -0,0 +1,13 @@
+package com.thamasia.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
