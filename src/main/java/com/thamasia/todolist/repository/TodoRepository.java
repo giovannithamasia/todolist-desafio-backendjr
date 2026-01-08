4a8eaa6c9b4cb11f85c4daad718cb73a58fec7e0
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-01-08 16:40:10 -0300

    feat: criar TodoRepository

---
+++ src/main/java/com/thamasia/todolist/repository/TodoRepository.java
@@ -0,0 +1,7 @@
+package com.thamasia.todolist.repository;
+
+import com.thamasia.todolist.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo,Long>{
+}
